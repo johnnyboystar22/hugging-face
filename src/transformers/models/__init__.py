a032dc96e1ee436dd1be236f8cf5d1c1969a32bb
steven <steven.bucaille@gmail.com>
2023-08-23 21:37:50 +0200

    Initial commit with template code generated by transformers-cli

--- src/transformers/models/__init__.py
+++ src/transformers/models/__init__.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from . import (
+    superglue,
     albert,
     align,
     altclip,
@@ -194,6 +195,7 @@ from . import (
     regnet,
     rembert,
     resnet,
+    superglue,
     roberta,
     roberta_prelayernorm,
     roc_bert,
--- src/transformers/models/auto/image_processing_auto.py
+++ src/transformers/models/auto/image_processing_auto.py
@@ -123,6 +123,7 @@ else:
             ("segformer", ("SegformerImageProcessor",)),
             ("seggpt", ("SegGptImageProcessor",)),
             ("siglip", ("SiglipImageProcessor",)),
+            ("superglue", "SuperGlueImageProcessor"),
             ("swiftformer", ("ViTImageProcessor", "ViTImageProcessorFast")),
             ("swin", ("ViTImageProcessor", "ViTImageProcessorFast")),
             ("swin2sr", ("Swin2SRImageProcessor",)),
