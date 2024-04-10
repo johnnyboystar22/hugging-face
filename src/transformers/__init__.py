ab9d88163bbffc3eae85a308b6b7df077854fb3f
steven <steven.bucaillle@gmail.com>
2024-04-11 00:21:36 +0200

    Deleted SuperGlueModel Fixed convert script

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -7516,7 +7516,6 @@ if TYPE_CHECKING:
         from .models.superglue import (
             SUPERGLUE_PRETRAINED_MODEL_ARCHIVE_LIST,
             SuperGlueForImageMatching,
-            SuperGlueModel,
             SuperGluePreTrainedModel,
         )
         from .models.superpoint import (
