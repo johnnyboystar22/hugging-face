dad79c3719e18022920e57b1df968147844f287d
ydshieh <ydshieh@users.noreply.github.com>
2024-04-16 13:51:23 +0200

    check llama

--- temp_llama.py
+++ temp_llama.py
@@ -1,4 +1,4 @@
 from transformers import LlamaForCausalLM, CodeLlamaTokenizer
 
-# tokenizer = CodeLlamaTokenizer.from_pretrained("codellama/CodeLlama-7b-hf")
+model = LlamaForCausalLM.from_pretrained("codellama/CodeLlama-7b-hf")
 model = LlamaForCausalLM.from_pretrained("codellama/CodeLlama-7b-hf")
