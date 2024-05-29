f79319c5aca0a91013e2e9e81f219c558b9cc6a4
jiqing-feng <jiqing.feng@intel.com>
2024-05-29 08:54:28 -0400

    fix 4 bit path

--- src/transformers/quantizers/quantizer_bnb_4bit.py
+++ src/transformers/quantizers/quantizer_bnb_4bit.py
@@ -58,8 +58,6 @@ class Bnb4BitHfQuantizer(HfQuantizer):
             self.modules_to_not_convert = self.quantization_config.llm_int8_skip_modules
 
     def validate_environment(self, *args, **kwargs):
-        if not torch.cuda.is_available():
-            raise RuntimeError("No GPU found. A GPU is needed for quantization.")
         if not (is_accelerate_available() and is_bitsandbytes_available()):
             raise ImportError(
                 "Using `bitsandbytes` 8-bit quantization requires Accelerate: `pip install accelerate` "
