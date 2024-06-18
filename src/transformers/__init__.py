134f33d1afe3ea91df53f332c43fa3bd724ec257
kamilakesbi <kamil@huggingface.co>
2024-06-18 18:51:38 +0200

    add feature extractor

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -310,7 +310,10 @@ _import_structure = {
         "CTRLTokenizer",
     ],
     "models.cvt": ["CvtConfig"],
-    "models.dac": ['DacConfig'],
+    "models.dac": [
+        'DacConfig', 
+        'DacFeatureExtractor'
+    ],
     "models.data2vec": [
         "Data2VecAudioConfig",
         "Data2VecTextConfig",
@@ -6388,6 +6391,7 @@ if TYPE_CHECKING:
         from .models.dac import (
             DacModel,
             DacPreTrainedModel,
+            DacFeatureExtractor, 
         )
         from .models.data2vec import (
             Data2VecAudioForAudioFrameClassification,
